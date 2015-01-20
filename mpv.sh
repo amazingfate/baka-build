e9617f61a0b09d8caf34031bcf65e5064e574ee1
amazingfate <liujianfeng1994@gmail.com>
2015-01-20 20:55:59 +0800

    update of script

--- mpv.sh
+++ mpv.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Build script for bakamplayer for native msys2/mingw-w64 by FrankHB.
-# based on official scripts.
-# Usage: ./mpv.sh $arch /e/Source/mpv
-# The 3rd argument is optional, using number of CPU cores if not preset.
 
 if [[ $1 == 'x86_64' ]]; then
 arch=x86_64
@@ -21,27 +17,18 @@ PREFIX=/mingw32
 fi
 
 if [[ $2 == '' ]]; then
-echo "Please specify mpv source directory."
-exit
-else
-mpv_src="$2"
-fi
+echo "Please specify mpv source direc
+# get mpv
+git clone https://github.com/mpv-player/mpv.git mpv.$arch
+cd mpv.$arch
+git pull
 
-if [[ $3 != '' ]]; then
-JOBS="$3"
-else
-JOBS=-j`grep -c ^processor /proc/cpuinfo`
-fi
-
-#pacman -S mingw-w64-$arch-ffmpeg mingw-w64-$arch-libass \
-#mingw-w64-$arch-libbluray mingw-w64-$arch-qt5-static --needed
 
 # build mpv
-pushd "$mpv_src"
 python3.3 bootstrap.py
-export PATH=$PREFIX/bin:$PATH
-export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
-DEST_OS=win32 TARGET=$arch-w64-mingw32 PKG_CONFIG=/usr/bin/pkg-config CC=gcc PERL=$PREFIX/bin/perl AR=$PREFIX/bin/ar \
+DEST_OS=win32 TARGET=$arch-w64-mingw32 PKG_CONFIG=/usr/bin/pkg-config \
+CC=gcc PERL=$PREFIX/bin/perl AR=$PREFIX/bin/ar \ 
+WINDRES=$PREFIX/bin/windres \
 python3.3 waf configure --check-c-compiler=gcc --prefix=$PREFIX \
 --enable-libmpv-shared --disable-client-api-examples
 python3.3 waf build $JOBS
@@ -54,10 +41,3 @@ sed -i 's_/instroot/bin_/$instroot/lib_' $instroot/lib/pkgconfig/mpv.pc
 rmdir $instroot/bin/pkgconfig
 popd
 
-# build baka-mplayer
-#QMAKE="$PREFIX/bin/qmake" \
-#./configure \
-#"CONFIG+=embed_translations" \
-#"lupdate=$PREFIX/bin/lupdate" \
-#"lrelease=$PREFIX/bin/lrelease"
-#make $JOBS
